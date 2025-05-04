38b113e5b275f270b0414d99d3001ce39324efad
Minyoung06 <monica_y@naver.com>
2025-05-04 22:05:50 +0900

    Feat: 라우터 페이지 및 404 설정 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router';
+
 import TestPage from '../views/TestPage.vue';
+
+import Home from '../views/HomePage.vue';
+import Search from '../views/SearchPage.vue';
+import Detail from '../views/DetailPage.vue';
+import Favorites from '../views/FavoritesPage.vue';
+import Profile from '../views/ProfilePage.vue';
+import Login from '../views/LoginPage.vue';
+import Signup from '../views/SignupPage.vue';
+import NotFound from '../views/NotFoundPage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'test',
-      component: TestPage,
-    },
+    { path: '/', name: 'Home', component: Home },
+    { path: '/search', name: 'Search', component: Search },
+    { path: '/detail/:id', name: 'Detail', component: Detail, props: true },
+    { path: '/favorites', name: 'Favorites', component: Favorites },
+    { path: '/profile', name: 'Profile', component: Profile },
+    { path: '/login', name: 'Login', component: Login },
+    { path: '/signup', name: 'Signup', component: Signup },
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
+    { path: '/test', name: 'test', component: TestPage },
   ],
 });
 
