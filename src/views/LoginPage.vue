32955cdab78dfdbc675145ea32f69edc8f451a1b
starwhale0w0 <starwhale@bu.ac.kr>
2025-05-08 17:06:02 +0900

    Feat: 로그인/회원가입 페이지

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,2 +1,53 @@
-<template></template>
+<template>
+  <div
+    class="d-flex justify-content-center align-items-center"
+    style="height: 100vh; background-color: #9a7dff"
+  >
+    <table>
+      <tr>
+        <img src="" />
+      </tr>
+      <tr>
+        <td><label for="id"> ID </label></td>
+        <td><input type="text" id="id" /></td>
+      </tr>
+      <tr>
+        <td><label for="password">Password </label></td>
+        <td><input type="password" id="password" /></td>
+      </tr>
+      <tr>
+        <td style="height: 35px"></td>
+      </tr>
+      <tr>
+        <td>
+          <button class="badge bg-white" style="color: black">로그인</button>
+        </td>
+        <td>
+          <router-link
+            to="/signup"
+            class="badge bg-white border border-black"
+            style="color: black; text-decoration: none"
+            >회원가입</router-link
+          >
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
 <script setup></script>
+
+<style scoped>
+tr {
+  color: white;
+}
+
+td {
+  width: 130px;
+  height: 40px;
+}
+
+input {
+  border-radius: 10px 10px;
+}
+</style>
