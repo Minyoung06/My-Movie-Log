1bdc052c1708cdcb18a2b289b873a75c31061986
Minyoung06 <monica_y@naver.com>
2025-06-17 13:40:33 +0900

    chore: demo 환경 구성

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/My-Movie-Log/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
